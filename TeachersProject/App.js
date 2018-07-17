1254f230820e8fea8f3e68b726232fa2a281a00c
Pedro Neto <pedro.neto@bemind.com.br>
2018-07-17 18:20:00 -0300

    create a fsimple latlist

--- TeachersProject/App.js
+++ TeachersProject/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, AppRegistry, Text, View, FlatList, Image } from 'react-native';
 
-export default class App extends React.Component {
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <FlatList
+          data={[
+            {name: 'Vinicius Cardoso', course: 'Sistemas de Informação', photo: 'https://cdn1.iconfinder.com/data/icons/occupations-3/100/21-512.png', bio: 'Professor de Engenharia de Software'},
+            {name: 'Carla Silva', course: 'Sistemas de Informação', photo: 'https://cdn3.iconfinder.com/data/icons/education-5-2/256/Teacher-512.png', bio: 'Professor de Engenharia de Software'}
+          ]}
+          renderItem={
+            ({item}) => 
+            <View style={styles.itemView}>
+              <Image source={{ uri: item.photo}} style={styles.photo}/>
+              <Text style={styles.itemList}>{item.name}</Text>
+            </View>
+          }
+        >
+        </FlatList>
       </View>
     );
   }
@@ -16,8 +28,31 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 40,
   },
+  itemView: {
+    flex: 1,
+    padding: 12,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  itemList: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    marginTop: 20,
+    marginLeft: 12
+  },
+  photo: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+  },
+  separator: {
+    flex: 1,
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#8E8E8E',
+  }
 });
+
+AppRegistry.registerComponent('TeachersProject', () => App);
